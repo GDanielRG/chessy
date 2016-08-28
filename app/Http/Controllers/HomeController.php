30f837f3cb9f5cd68a03199c69f45aa7d1b54628
GDanielRG <danyrod94@gmail.com>
2016-08-27 19:21:35 -0500

    change

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,7 +17,7 @@ class HomeController extends Controller
         \Log::info($request);
         $name= base_convert(mt_rand (1, 1125899906842623), 10, 32) . ".html";
         echo('name= ' . $name);
-        $path= public_path() . '\\' . "images". '\\'. $name;
+        $path= public_path() . '\\'. $name;
         $myfile = fopen($path, "w") or die("Unable to open file!");
         fwrite($myfile, $this->generateGrid());
         fclose($myfile);
@@ -28,7 +28,7 @@ class HomeController extends Controller
 
 
         return response()->json([
-            'path' => url("/images/" . $name ),
+            'path' => url("/" . $name ),
         ]);
 
     }
